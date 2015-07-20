3540e247f00cd811fd8ec70f8cf96c90dbea7db3
Jesse Shawl <jesse@jshawl.com>
2015-07-20 17:24:48 -0400

    add solution

--- app.rb
+++ app.rb
@@ -1,2 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+get "/" do
+  "99 bottles of beer on the wall. <a href='/bottles/98'>Take one Down</a>"
+end
+
+get "/bottles/:num" do
+  num = params[:num].to_i
+  if num == 0
+    "No bottles of beer on the wall. <a href='/bottles/99'>Start Over</a>"
+  else
+    "#{num} bottles of beer on the wall. <a href='/bottles/#{num-1}'>Take one Down</a>"
+  end
+end
